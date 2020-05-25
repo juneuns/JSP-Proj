ce495de52833c9604091c9ad4e6bee9ad3c71f16
Gwangho27 <khpark0227@daum.net>
2020-05-25 16:20:09 +0900

    다 지워버림

--- proj/WebContent/js/sideBar.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$('#fbtn').click(function(){
-		$(location).attr('href', '/proj/board/boardFlist.do');
-	});
-	
-	$('#tbtn').click(function(){
-		$(location).attr('href', '/proj/board/boardTlist.do');
-	});
-});
